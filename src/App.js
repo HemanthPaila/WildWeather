15e41b92dd75082c212cb747a5dfeeb00e7964cd
HemanthPaila <pailahemanth2002@gmail.com>
2023-03-07 22:47:53 +0530

    completed search.js component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Search from "./components/search/search";
+import "./App.css";
+
+function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "cfb0807c60msh0d2399b729436f2p17b30cjsnb7e87deb1ff4",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
